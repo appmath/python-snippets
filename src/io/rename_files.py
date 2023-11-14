614c6dd36d565e7385c2350b9cc529b2af0ec788
saadkadhi2 <littlebigartist@gmail.com>
2023-11-13 19:43:35 -0700

    First push

--- src/io/rename_files.py
+++ src/io/rename_files.py
@@ -1,25 +1,34 @@
 import os
+import re
 
 
-def add_a_to_filenames(folder_path):
-    # Check if the provided path is a directory
-    if not os.path.isdir(folder_path):
-        print("The provided path is not a directory.")
-        return
-
-    # Iterate over all files in the directory
+def rename_java_files(folder_path):
     for filename in os.listdir(folder_path):
-        old_file = os.path.join(folder_path, filename)
-
-        # Check if it's a file and not a directory
-        if os.path.isfile(old_file):
+        if filename.endswith(".java"):
+            # Construct new file name
             new_filename = 'A' + filename
-            new_file = os.path.join(folder_path, new_filename)
+            new_file_path = os.path.join(folder_path, new_filename)
+            old_file_path = os.path.join(folder_path, filename)
 
-            # Rename the file
-            os.rename(old_file, new_file)
-            print(f"Renamed {filename} to {new_filename}")
+            # Read the old file
+            with open(old_file_path, 'r') as file:
+                filedata = file.read()
+
+            # Find the class name and replace it
+            # Assuming the file name (without .java) is the class name
+            class_name = filename[:-5]
+            new_class_name = 'A' + class_name
+            filedata = re.sub(r'\b' + class_name + r'\b', new_class_name, filedata)
+
+            # Write the changes to the new file
+            with open(new_file_path, 'w') as file:
+                file.write(filedata)
+
+            # Remove the old file
+            os.remove(old_file_path)
+
+            print(f"Renamed {filename} to {new_filename} and updated class name to {new_class_name}")
 
 
-# Replace 'your_directory_path' with the path to the directory containing the files you want to rename
-add_a_to_filenames('your_directory_path')
+folder_path = '/path/to/your/folder'  # Replace with your folder path
+rename_java_files(folder_path)
