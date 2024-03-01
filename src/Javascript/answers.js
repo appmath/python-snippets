806cd0ee68443f21d2e0bc07da35537ab15c8e04
saadkadhi2 <littlebigartist@gmail.com>
2024-02-29 19:40:34 -0700

    OKTA ID generator

---
+++ src/Javascript/answers.js
@@ -0,0 +1,11 @@
+function generateRandomOktaId() {
+    const length = 20; // Typical length of an Okta ID
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+console.log(generateRandomOktaId());
