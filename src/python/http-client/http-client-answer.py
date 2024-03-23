29592f129c14b08c6174dc36101d35a9834fd483
saadkadhi2 <littlebigartist@gmail.com>
2024-03-23 10:42:31 -0700

    OKTA ID generator

--- src/python/http-client/http-client-answer.py
+++ src/python/http-client/http-client-answer.py
@@ -1,18 +1,14 @@
 import json
 import re
+from icecream import ic
 
 
+# Function to replace environment numbers in URLs
 def replace_env_in_url(text, target_env):
-    """
-    Replace the environment number in URLs within the given text.
-    """
     return re.sub(r'(https://url-env)(\d)', r'\g<1>' + target_env, text)
 
 
-def process_input_file(input_filename):
-    """
-    Read and process the input file, then generate a JSON structure with environment-specific content.
-    """
+def generate_env_json(input_filename):
     # Read the input file
     with open(input_filename, 'r') as file:
         input_content = file.read()
@@ -35,32 +31,18 @@ def process_input_file(input_filename):
         # Assign the adjusted content to the current environment
         data[env] = adjusted_content
 
-    return data
-
-
-def write_to_json_file(data, output_filename='http.client.env.json'):
-    """
-    Write the given data to a JSON file.
-    """
-    with open(output_filename, 'w') as json_file:
+    # Write the dictionary to a JSON file
+    with open('http.client.env.json', 'w') as json_file:
         json.dump(data, json_file, indent=4)
 
+    ic("JSON file 'http.client.env.json' has been generated.")
 
-def main(input_filename):
-    """
-    Main function to orchestrate reading, processing, and writing the environment-specific data.
-    """
-    # Process the input file to generate the environment-specific data
-    data = process_input_file(input_filename)
 
-    # Write the processed data to a JSON file
-    write_to_json_file(data)
-
-    # Fun console output
-    print("ice cream")
+def main():
+    # Define the name of the input file here
+    input_filename = 'input.txt'
+    generate_env_json(input_filename)
 
 
 if __name__ == "__main__":
-    # Define the name of the input file here
-    input_filename = 'input.txt'
-    main(input_filename)
+    main()
