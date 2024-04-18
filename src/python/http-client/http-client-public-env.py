fa7cf1a4b448129142db847de5dd37db74448a6c
saadkadhi2 <littlebigartist@gmail.com>
2024-04-18 10:20:14 -0700

    OKTA ID generator

--- src/python/http-client/http-client-public-env.py
+++ src/python/http-client/http-client-public-env.py
@@ -9,9 +9,7 @@ def read_and_fix_input(input_filename):
     """
     try:
         with open(input_filename, 'r') as file:
-            # Assuming the entire file content is a single, albeit incorrectly formatted, JSON object
             content = file.read().strip()
-            # Attempt to fix common JSON issues (this is a naive approach and might not work for all cases)
             fixed_content = "{" + content + "}"
             return fixed_content
     except Exception as e:
@@ -21,12 +19,15 @@ def read_and_fix_input(input_filename):
 
 def replace_env_in_url(text, env):
     """
-    Replace '-envX' with '-envY' where Y is the target environment number or name.
-    For 'perf' and 'prep' environments, changes "71b" to "71d".
+    Replace '-envX' with either '-envY' where Y is the target environment number,
+    or 'prep' or 'perf' directly if the env is 'prep' or 'perf'.
     """
-    text = re.sub(r'-env\d+', f'-env{env.replace("env", "")}', text)
+    new_env = env.replace("env", "")
     if env in ['perf', 'prep']:
-        text = text.replace("71b", "71d")  # Change "71b" to "71d" for specific environments
+        text = re.sub(r'-env\d+', f'-{new_env}', text)
+        text = text.replace("71b", "71d")
+    else:
+        text = re.sub(r'-env\d+', f'-env{new_env}', text)
     return text
 
 
@@ -35,13 +36,11 @@ def process_input_content(input_content):
     Process the input content to replace environment numbers in URLs
     and organize them into a structured dictionary, except for the "Security" section.
     """
-    envs = ['env1', 'env2', 'env3', 'env4', 'env5', 'env6', 'env7', 'perf', 'prep']
+    envs = ['local', 'env1', 'env2', 'env3', 'env4', 'env5', 'env6', 'env7', 'perf', 'prep']
     env_structure = {env: {} for env in envs}
 
     try:
         input_data = json.loads(input_content)
-
-        # Check if the "Security" section exists
         security_data = input_data.pop("Security", None)
 
         for env in envs:
@@ -49,7 +48,6 @@ def process_input_content(input_content):
                 if isinstance(value, str):
                     adjusted_value = replace_env_in_url(value, env)
                     env_structure[env][key] = adjusted_value
-            # Copy the "Security" section as is, if it exists
             if security_data is not None:
                 env_structure[env]["Security"] = security_data
 
@@ -78,10 +76,9 @@ def generate_env_json(input_filename, output_filename):
 
 
 def main():
-    input_filename = 'env.txt'
-    output_filename = 'http.client.env.json'
+    input_filename = 'public-env-input.txt'
+    output_filename = 'http-client.env.json'
     generate_env_json(input_filename, output_filename)
-
 
 if __name__ == "__main__":
     main()
