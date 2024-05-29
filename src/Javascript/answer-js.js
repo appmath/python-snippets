cd618c9cd94956de105a2286bc334364797f049c
appmath <azeez.kadhi@gmailcom>
2024-05-29 15:19:42 -0700

    Randome email addressess

--- src/Javascript/answer-js.js
+++ src/Javascript/answer-js.js
@@ -1,18 +1,13 @@
-function generateRandomEmail(domainList = ["example.com", "test.com", "sample.org"], numEmails = 1) {
-    const emails = [];
-
-    for (let i = 0; i < numEmails; i++) {
-        const usernameLength = Math.floor(Math.random() * (12 - 6 + 1)) + 6;
-        const username = Array.from({ length: usernameLength }, () =>
-            String.fromCharCode(Math.floor(Math.random() * 36) + (Math.random() < 0.5 ? 48 : 87))
-        ).join('');
-        const domain = domainList[Math.floor(Math.random() * domainList.length)];
-        emails.push(`${username}@${domain}`);
-    }
-
-    return emails;
+function generateRandomEmail() {
+    const domains = ["example.com", "test.com", "sample.org"];
+    const usernameLength = Math.floor(Math.random() * (12 - 6 + 1)) + 6;
+    const username = Array.from({ length: usernameLength }, () =>
+        String.fromCharCode(Math.floor(Math.random() * 36) + (Math.random() < 0.5 ? 48 : 87))
+    ).join('');
+    const domain = domains[Math.floor(Math.random() * domains.length)];
+    return `${username}@${domain}`;
 }
 
-// Generate 10 random email addresses
-const randomEmails = generateRandomEmail(undefined, 10);
-randomEmails.forEach(email => console.log(email));
+// Generate a random email address
+const randomEmail = generateRandomEmail();
+console.log(randomEmail);
